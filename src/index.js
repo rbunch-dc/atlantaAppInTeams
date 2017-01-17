d20501b16a2787399dbd57097bb804a350650b8d
Ryan Ramdehol <rramdeholshift@gmail.com>
2017-01-17 14:29:55 -0500

    deleted line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import './index.css';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import About from './About';
